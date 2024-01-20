dd905d85c712bdc21f1066536188387358fa3e81
LiamBa99 <liambadge@gmail.com>
2024-01-20 14:00:53 -0800

    Added no data case for tables

--- client/index.js
+++ client/index.js
@@ -3,14 +3,18 @@ document.addEventListener('DOMContentLoaded', function () {
     //.then(response => response.json())
     //.then(data => loadHTMLTable(data['data']));
 
-    loadHtmlTables([]);
+    loadHTMLTables([]);
 
 });
 
 function loadHTMLTables(data) {
-    const schoolTable = document.querySelector('#school-table', '#school-body');
+    const schoolTable = document.querySelector('#school-table #school-body');
+    const teacherTable = document.querySelector('#teacher-table #teacher-body');
+    const studentTable = document.querySelector('#student-table #student-body');
 
     if (data.length === 0) {
-        schoolTable.innerHTML = "<tr><td class = 'no-data'  colspan = '2'>No Data</td></tr>"
-    }
+        schoolTable.innerHTML = "<tr><td class='no-data' colspan='2'>No Data</td></tr>"
+        teacherTable.innerHTML = "<tr><td class='no-data' colspan='3'>No Data</td></tr>"
+        studentTable.innerHTML = "<tr><td class='no-data' colspan='7'>No Data</td></tr>"
+    }   
 }
