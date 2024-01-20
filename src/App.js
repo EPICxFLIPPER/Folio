4e5fa7edc3705d566feaf2904d05f7112341e293
Dom4489 <lhuang32@student.ubc.ca>
2024-01-20 12:48:34 -0800

    setup frontend structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import frontPage from "./components/frontPage";
+
+const App = () => {
+  return (
+      <div>
+        {/* front page */}
+        <frontPage />
+      </div>
+  );
+};
+
+export default App;;
---
+++ src/components/frontPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./frontPage.css" ;
+
+const frontPage = () => {
+ return {
+   
+ }
+}
