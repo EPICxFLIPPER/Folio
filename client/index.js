9482be5d1d024cef01de366632913597a01fffd0
EPICxFLIPPER <ryanfraser832@icloud.com>
2024-01-20 13:53:07 -0800

    Added functionality for no data

--- client/index.js
+++ client/index.js
@@ -1,5 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('http://localhost:5000/getAll')
+    // fetch('http://localhost:5000/getAll')
     //.then(response => response.json())
     //.then(data => loadHTMLTable(data['data']));
+
+    loadHtmlTables([]);
+
 });
+
+function loadHTMLTables(data) {
+    const schoolTable = document.querySelector('#school-table', '#school-body');
+
+    if (data.length === 0) {
+        schoolTable.innerHTML = "<tr><td class = 'no-data'  colspan = '2'>No Data</td></tr>"
+    }
+}
