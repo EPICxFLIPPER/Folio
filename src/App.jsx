bfd48182ee3a26a00d59dfb296a3a36ead1994ed
victoriahhu <74104000+victoriahhu@users.noreply.github.com>
2024-01-21 08:19:17 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import StudentInfo from "./components/studentInfo/frontPage";
+import StudentInfo from "./components/studentInfo";
 import ClassView from "./components/classView"; // Capitalized the component name
 
 const App = () => {
