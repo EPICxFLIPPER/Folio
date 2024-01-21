dfa27fff761a461784a48a90484deba294f997fc
LiamBa99 <liambadge@gmail.com>
2024-01-21 07:56:10 -0800

    Added on click for students

--- src/components/classView.jsx
+++ src/components/classView.jsx
@@ -46,54 +46,57 @@ for (let j = counter; j < 30; j++) {
   studentNames[j] = "";
 }
 
+const handleButtonClick = (id) => {
+  console.log(`Button clicked for ${id}`);
+}
  return (
   <div className>
        {/* start of students */}
        <div className="students">
         {/* start of row 1 */}
         <div className="row">
-         <div className="studentBtn"><button> {studentNames[0]} </button></div>
-         <div className="studentBtn"><button> {studentNames[1]} </button></div>
-         <div className="studentBtn"><button> {studentNames[2]} </button></div>
-         <div className="studentBtn"><button> {studentNames[3]} </button></div>
-         <div className="studentBtn"><button> {studentNames[4]}</button></div>
-         <div className="studentBtn"><button> {studentNames[5]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[0].StudentId)}> {studentNames[0]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[1].StudentId)}> {studentNames[1]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[2].StudentId)}> {studentNames[2]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[3].StudentId)}> {studentNames[3]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[4].StudentId)}> {studentNames[4]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[5].StudentId)}> {studentNames[5]}</button></div>
         </div>
          {/* start of row 2 */}
          <div className="row">
-         <div className="studentBtn"><button> {studentNames[6]}</button></div>
-         <div className="studentBtn"><button> {studentNames[7]}</button></div>
-         <div className="studentBtn"><button> {studentNames[8]}</button></div>
-         <div className="studentBtn"><button> {studentNames[9]}</button></div>
-         <div className="studentBtn"><button> {studentNames[10]}</button></div>
-         <div className="studentBtn"><button> {studentNames[11]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[6].StudentId)}> {studentNames[6]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[7].StudentId)}> {studentNames[7]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[8].StudentId)}> {studentNames[8]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[9].StudentId)}> {studentNames[9]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[10].StudentId)}> {studentNames[10]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[11].StudentId)}> {studentNames[11]}</button></div>
          </div>
           {/* start of row 3 */}
         <div className="row">
-        <div className="studentBtn"><button> {studentNames[12]}</button></div>
-         <div className="studentBtn"><button> {studentNames[13]}</button></div>
-         <div className="studentBtn"><button> {studentNames[14]}</button></div>
-         <div className="studentBtn"><button> {studentNames[15]}</button></div>
-         <div className="studentBtn"><button> {studentNames[16]}</button></div>
-         <div className="studentBtn"><button> {studentNames[17]}</button></div>
+        <div className="studentBtn"><button onClick={() => handleButtonClick(names[12].StudentId)}> {studentNames[12]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[13].StudentId)}> {studentNames[13]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[14].StudentId)}> {studentNames[14]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[15].StudentId)}> {studentNames[15]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[16].StudentId)}> {studentNames[16]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[17].StudentId)}> {studentNames[17]}</button></div>
         </div>
         {/* start of row 4 */}
         <div className="row">
-        <div className="studentBtn"><button> {studentNames[18]}</button></div>
-         <div className="studentBtn"><button> {studentNames[19]}</button></div>
-         <div className="studentBtn"><button> {studentNames[20]}</button></div>
-         <div className="studentBtn"><button> {studentNames[21]}</button></div>
-         <div className="studentBtn"><button> {studentNames[22]}</button></div>
-         <div className="studentBtn"><button> {studentNames[23]}</button></div>
+        <div className="studentBtn"><button onClick={() => handleButtonClick(names[18].StudentId)}> {studentNames[18]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[19].StudentId)}> {studentNames[19]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[20].StudentId)}> {studentNames[20]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[21].StudentId)}> {studentNames[21]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[22].StudentId)}> {studentNames[22]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[23].StudentId)}> {studentNames[23]}</button></div>
         </div>
           {/* start of row 5 */}
         <div className="row">
-        <div className="studentBtn"><button> {studentNames[24]}</button></div>
-         <div className="studentBtn"><button> {studentNames[25]}</button></div>
-         <div className="studentBtn"><button> {studentNames[26]}</button></div>
-         <div className="studentBtn"><button> {studentNames[27]}</button></div>
-         <div className="studentBtn"><button> {studentNames[28]}</button></div>
-         <div className="studentBtn"><button> {studentNames[29]}</button></div>
+        <div className="studentBtn"><button onClick={() => handleButtonClick(names[24].StudentId)}> {studentNames[24]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[25].StudentId)}> {studentNames[25]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[26].StudentId)}> {studentNames[26]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[27].StudentId)}> {studentNames[27]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[28].StudentId)}> {studentNames[28]}</button></div>
+         <div className="studentBtn"><button onClick={() => handleButtonClick(names[29].StudentId)}> {studentNames[29]}</button></div>
         </div>
        </div>
        {/* end of students */}
