936495554b70fbe7411743bd96eb783b9a994308
Dom4489 <lhuang32@student.ubc.ca>
2024-01-20 22:22:46 -0800

    beginning to connect front end to backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import FrontPage from "./components/frontPage/frontPage";
 import ClassView from "./components/classView"; // Capitalized the component name
 
 const App = () => {
   return (
-    // <FrontPage />
-    <ClassView />
+  //   <FrontPage />
+    <ClassView/>
+    // <FrontPage/>
   );
 };
 
--- src/components/classView.jsx
+++ src/components/classView.jsx
@@ -2,54 +2,60 @@ import React from "react";
 import "./classView.css";
 
 const classView = () => {
+const names = ["1", "2", "3", "4", "5", "6",
+"7", "8", "3", "4", "5", "6",
+"1", "2", "3", "4", "5", "6",
+"1", "2", "3", "4", "5", "6",
+"1", "2", "3", "4", "5", "6",];
+
  return (
   <div className>
        {/* start of students */}
        <div className="students">
         {/* start of row 1 */}
         <div className="row">
-         <div className="studentBtn"><button>name</button></div>
-         <div className="studentBtn"><button>name</button></div>
-         <div className="studentBtn"><button>name</button></div>
-         <div className="studentBtn"><button>name</button></div>
-         <div className="studentBtn"><button>name</button></div>
-         <div className="studentBtn"><button>name</button></div>
+         <div className="studentBtn"><button> {names[0]} </button></div>
+         <div className="studentBtn"><button> {names[1]} </button></div>
+         <div className="studentBtn"><button> {names[2]} </button></div>
+         <div className="studentBtn"><button> {names[3]} </button></div>
+         <div className="studentBtn"><button> {names[4]}</button></div>
+         <div className="studentBtn"><button> {names[5]}</button></div>
         </div>
          {/* start of row 2 */}
          <div className="row">
-         <div className="studentBtn"><button>name</button></div>
-         <div className="studentBtn"><button>name</button></div>
-         <div className="studentBtn"><button>name</button></div>
-         <div className="studentBtn"><button>name</button></div>
-         <div className="studentBtn"><button>name</button></div>
-         <div className="studentBtn"><button>name</button></div>
+         <div className="studentBtn"><button> {names[6]}</button></div>
+         <div className="studentBtn"><button> {names[7]}</button></div>
+         <div className="studentBtn"><button> {names[8]}</button></div>
+         <div className="studentBtn"><button> {names[9]}</button></div>
+         <div className="studentBtn"><button> {names[10]}</button></div>
+         <div className="studentBtn"><button> {names[11]}</button></div>
          </div>
           {/* start of row 3 */}
         <div className="row">
-        <div className="studentBtn"><button>name</button></div>
-         <div className="studentBtn"><button>name</button></div>
-         <div className="studentBtn"><button>name</button></div>
-         <div className="studentBtn"><button>name</button></div>
-         <div className="studentBtn"><button>name</button></div>
-         <div className="studentBtn"><button>name</button></div>
+        <div className="studentBtn"><button> {names[12]}</button></div>
+         <div className="studentBtn"><button> {names[13]}</button></div>
+         <div className="studentBtn"><button> {names[14]}</button></div>
+         <div className="studentBtn"><button> {names[15]}</button></div>
+         <div className="studentBtn"><button> {names[16]}</button></div>
+         <div className="studentBtn"><button> {names[17]}</button></div>
         </div>
         {/* start of row 4 */}
         <div className="row">
-        <div className="studentBtn"><button>name</button></div>
-         <div className="studentBtn"><button>name</button></div>
-         <div className="studentBtn"><button>name</button></div>
-         <div className="studentBtn"><button>name</button></div>
-         <div className="studentBtn"><button>name</button></div>
-         <div className="studentBtn"><button>name</button></div>
+        <div className="studentBtn"><button> {names[18]}</button></div>
+         <div className="studentBtn"><button> {names[19]}</button></div>
+         <div className="studentBtn"><button> {names[20]}</button></div>
+         <div className="studentBtn"><button> {names[21]}</button></div>
+         <div className="studentBtn"><button> {names[22]}</button></div>
+         <div className="studentBtn"><button> {names[23]}</button></div>
         </div>
           {/* start of row 5 */}
         <div className="row">
-        <div className="studentBtn"><button>name</button></div>
-         <div className="studentBtn"><button>name</button></div>
-         <div className="studentBtn"><button>name</button></div>
-         <div className="studentBtn"><button>name</button></div>
-         <div className="studentBtn"><button>name</button></div>
-         <div className="studentBtn"><button>name</button></div>
+        <div className="studentBtn"><button> {names[24]}</button></div>
+         <div className="studentBtn"><button> {names[25]}</button></div>
+         <div className="studentBtn"><button> {names[26]}</button></div>
+         <div className="studentBtn"><button> {names[27]}</button></div>
+         <div className="studentBtn"><button> {names[28]}</button></div>
+         <div className="studentBtn"><button> {names[29]}</button></div>
         </div>
        </div>
        {/* end of students */}
